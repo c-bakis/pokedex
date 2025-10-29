8e968d37a23101c76c04cc4ac0942378d6c22d93
Christina Bakis <c.bakis@gmx.de>
2025-10-29 13:05:26 +0100

    feat: fetch and render pokemon and details from API

--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,36 @@
     let pokemonList = [];
 
     async function loadPokemon() {
-        let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=30&offset=0');
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=40&offset=0');
         let data = await response.json();
-        pokemonList = data.results.map(pokemon => ({
-            name: pokemon.name,
-            url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonIdFromUrl(pokemon.url)}.png`
-        }));
+        await renderAllPokemon(data.results);
+    }
+
+    async function loadPokemonDetails(pokemon) {
+        let response = await fetch(pokemon.url);
+        let data = await response.json();
+        let pokemonData = {
+            id: data.id,
+            name: data.name,
+            image: data.sprites.front_default,
+            types: data.types.map(typeInfo => typeInfo.type.name)
+        };
+        return pokemonData;
+    }
+
+    let sortPokemonList = () => {
+        pokemonList.sort((a, b) => a.id - b.id);
+        return pokemonList;
+    }
+
+    let renderAllPokemon = async (pokemons) => {
+        const results = await Promise.all(pokemons.map(pokemon => loadPokemonDetails(pokemon)));
+        pokemonList = results;
+
+        sortPokemonList();
+
         pokemonContainer.innerHTML = '';
         pokemonList.forEach(pokemon => {
             pokemonContainer.innerHTML += renderPokemonCard(pokemon);
         });
-    }
-
-    function getPokemonIdFromUrl(url) {
-      // z.B. "https://pokeapi.co/api/v2/pokemon/25/" -> "25"
-      const parts = url.split('/').filter(Boolean); // entfernt leere Segmente
-      return parts[parts.length - 1]; // letzte Komponente ist die ID
     }
--- scripts/template.js
+++ scripts/template.js
@@ -2,9 +2,15 @@
 let renderPokemonCard = (pokemon) => {
     console.log('Rendering card for:', pokemon);
     return `
-        <div class="pokemon-card">
-            <h3>${pokemon.name}</h3>
-            <img src="${pokemon.url}" alt="${pokemon.name} image">
-        </div>
+                 <div class="pokemon-card">
+                    <div class="pokemon-name-number">
+                        <h3>
+                            <span class="pokemon-number">#${pokemon.id}</span>
+                        <span class="pokemon-name">${pokemon.name}</span>
+                    </h3>
+                    <img src="${pokemon.image}" alt="${pokemon.name} image" class="pokemon-image">
+                    <div class="pokemon-types">${pokemon.types}</div>
+                    </div>
+                 </div>
     `;
 }
